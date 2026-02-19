30bfa592449d8cf9c7879bb823ad2fd970aced2b
Ayush78707 <ayush78707@gmail.com>
2026-02-19 14:36:13 +0530

    Update qudratic.c

--- qudratic.c
+++ qudratic.c
@@ -1,23 +1,25 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int a,b ,c ,d ;
-    int x;
-    printf("Enter value of a,b,c:");
-    scanf("%d%d%d",&a,&b,&c);
-    d=sqrt((b*b)-(4*a*c));
-    if(d>=0){
-        printf("two real and distinct roots:");
-        printf("%d%d\n",x=(-b+sqrt(d))/(2*a));
-        printf("%d%d",x=(-b-sqrt(d))/2*a);
+#include <stdio.h>
+#include <math.h>  // Required for sqrt() function
+
+int main() {
+    float a, b, c, discriminant, root1, root2, realPart, imaginaryPart;
+    printf("Enter coefficients a, b, and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Roots are real and distinct: %.2f and %.2f", root1, root2);
+    } else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("Roots are real and equal: %.2f and %.2f", root1, root2);
+    } else {
+        realPart = -b / (2 * a);
+        imaginaryPart = sqrt(-discriminant) / (2 * a);
+        printf("Roots are complex: %.2f + %.2fi and %.2f - %.2fi", realPart, imaginaryPart, realPart, imaginaryPart);
     }
-    else if (d==0){
-        printf("TWO equal roots:");
-        printf("%d%d\n",x=(-b+sqrt(d))/(2*a));
-        printf( "%d%d",x=(-b-sqrt(d))/(2*a));
-    }
-    else{
-        printf("Roots are complex");
-    }
+
     return 0;
 }
